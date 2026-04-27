428be0057349f8fd4736bf33029441fb96ec790c
isnabok <isnabok@gmail.com>
2026-04-27 21:20:13 +0400

    fixed database connect v3

--- app/database.py
+++ app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/data/tds.db")
 
+os.makedirs("/app/data", exist_ok=True)
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
